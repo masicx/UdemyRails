f5c77de1a6534b695bdbf911a55434e6f193a023
Marthell Iñiguez Cossio <marthell@Marthells-Mac.local>
2017-07-30 09:32:27 -0500

    Números, operadores aritméticos, operadores logicos

---
+++ Ejemplo/numbers.rb
@@ -0,0 +1,21 @@
+puts " -- Operaciones Matemáticas"
+puts 1 + 2
+puts 3 * 5
+puts 20 / 3 # División de enteros no retorna valores decimales
+# ---------------------------------------------------------
+puts " -- Flotantes"
+# Hay varias formas de hacer operaciones que retornen decimales
+puts 20 / 3.to_f # Convertir a float
+puts 20.0 / 3 # Poner decimales
+# ---------------------------------------------------------
+puts " -- Mod "
+puts 21 % 2
+puts 22 % 2
+puts 23 % 2
+puts " -- Pares e impares"
+puts  23.odd? # impar?
+puts 23.even? # par?
+# ---------------------------------------------------------
+puts " -- Operadores Logicos"
+puts 10 < 1
+puts 9 > 4
