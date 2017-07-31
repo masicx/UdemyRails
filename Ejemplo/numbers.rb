d2e988cc06010f042ac8de61ef7658bf244e0fb6
Marthell Iñiguez Cossio <marthell@Marthells-Mac.local>
2017-07-30 22:34:09 -0500

    Conversiones y random

--- Ejemplo/numbers.rb
+++ Ejemplo/numbers.rb
@@ -1,21 +1,32 @@
-puts " -- Operaciones Matemáticas"
-puts 1 + 2
-puts 3 * 5
-puts 20 / 3 # División de enteros no retorna valores decimales
+# puts " -- Operaciones Matemáticas"
+# puts 1 + 2
+# puts 3 * 5
+# puts 20 / 3 # División de enteros no retorna valores decimales
 # ---------------------------------------------------------
-puts " -- Flotantes"
+# puts " -- Flotantes"
 # Hay varias formas de hacer operaciones que retornen decimales
-puts 20 / 3.to_f # Convertir a float
-puts 20.0 / 3 # Poner decimales
+# puts 20 / 3.to_f # Convertir a float
+# puts 20.0 / 3 # Poner decimales
 # ---------------------------------------------------------
-puts " -- Mod "
-puts 21 % 2
-puts 22 % 2
-puts 23 % 2
-puts " -- Pares e impares"
-puts  23.odd? # impar?
-puts 23.even? # par?
+# puts " -- Mod "
+# puts 21 % 2
+# puts 22 % 2
+# puts 23 % 2
+# puts " -- Pares e impares"
+# puts  23.odd? # impar?
+# puts 23.even? # par?
 # ---------------------------------------------------------
-puts " -- Operadores Logicos"
-puts 10 < 1
-puts 9 > 4
+# puts " -- Operadores Logicos"
+# puts 10 < 1
+# puts 9 > 4
+# Random
+# ---------------------------------------------------------
+puts " -- Random"
+puts rand(10)
+# ---------------------------------------------------------
+puts " -- Conversiones"
+# String a Integer
+"5".to_i
+# Object a String
+5.to_s
+
